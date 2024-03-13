230f704b3395091bb1ed7ed9d62a6f13f3781c04
AJ5154 <ashishjadhav597@gmail.com>
2024-03-13 18:43:19 +0530

    line chart is created

--- src/features/cryptoCoins/bitcoins/components/BitcoinOverview.tsx
+++ src/features/cryptoCoins/bitcoins/components/BitcoinOverview.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import { CryptoIcon, CurrencyCode } from "@/components/Elements";
+import { ArrowDropUp } from "@mui/icons-material";
+import {
+  Chip,
+  Container,
+  Divider,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { CreateChart } from "./CreateChart";
 
 export const BitcoinOverview = () => {
-  return <div>BitcoinOverview</div>;
+  return (
+    <Container component="div">
+      <Paper elevation={2}>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="h4">
+            <CryptoIcon currencyCode={CurrencyCode.BTC} />
+            Bitcoin
+          </Typography>
+          <Typography variant="h6">BTC</Typography>
+          <Chip label="#Rank 1" />
+        </Stack>
+
+        <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
+          <Typography variant="h4">$46,953.04</Typography>
+          <Typography variant="h6">
+            <ArrowDropUp sx={{ verticalAlign: "middle" }} />
+            2.51%
+          </Typography>
+          <Typography variant="h6">(24H)</Typography>
+        </Stack>
+        <Divider />
+        <Stack direction="row" spacing={2}>
+          <Typography variant="h5" sx={{ textTransform: "capitalize" }}>
+            Bitcoin Price Chart (USD)
+          </Typography>
+          <Chip label="1H" />
+          <Chip label="24H" />
+          <Chip label="7D" />
+          <Chip label="1M" />
+          <Chip label="3M" />
+          <Chip label="6M" />
+          <Chip label="1Y" />
+          <Chip label="ALL" />
+        </Stack>
+        <CreateChart />
+      </Paper>
+    </Container>
+  );
 };
--- src/features/cryptoCoins/bitcoins/routes/KoinxUI.tsx
+++ src/features/cryptoCoins/bitcoins/routes/KoinxUI.tsx
@@ -1,10 +1,17 @@
 import { ContentLayout } from "@/components/Layout";
 import { BitcoinBreadCrumb } from "../components/BitcoinBreadCrumb";
+import { Grid, Stack } from "@mui/material";
+import { BitcoinOverview } from "../components/BitcoinOverview";
 
 export const KoinxUI = () => {
   return (
     <ContentLayout title="">
       <BitcoinBreadCrumb />
+      <Grid spacing={2}>
+        <Stack direction="column" spacing={2}>
+          <BitcoinOverview />
+        </Stack>
+      </Grid>
     </ContentLayout>
   );
 };
