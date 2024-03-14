708a6e04b249c8fc62aac27cd8b440cd4e49f6c4
AJ5154 <ashishjadhav597@gmail.com>
2024-03-14 11:02:39 +0530

    mapping is added for chip and card is used instead of paper

--- src/features/cryptoCoins/bitcoins/components/BitcoinOverview.tsx
+++ src/features/cryptoCoins/bitcoins/components/BitcoinOverview.tsx
@@ -1,52 +1,52 @@
 import { CryptoIcon, CurrencyCode } from "@/components/Elements";
 import { ArrowDropUp } from "@mui/icons-material";
 import {
+  Card,
+  CardContent,
   Chip,
   Container,
   Divider,
-  Paper,
   Stack,
   Typography,
 } from "@mui/material";
 import { CryptoPriceLineChart } from "./CryptoPriceLineChart";
 
 export const BitcoinOverview = () => {
+  const chipLabels = ["1H", "24H", "7D", "1M", "3M", "6M", "1Y", "ALL"];
+
   return (
     <Container component="div">
-      <Paper elevation={2}>
-        <Stack direction="row" spacing={2}>
-          <Typography variant="h4">
-            <CryptoIcon currencyCode={CurrencyCode.BTC} />
-            Bitcoin
-          </Typography>
-          <Typography variant="h6">BTC</Typography>
-          <Chip label="#Rank 1" />
-        </Stack>
+      <Card variant="outlined">
+        <CardContent>
+          <Stack direction="row" spacing={2}>
+            <Typography variant="h4">
+              <CryptoIcon currencyCode={CurrencyCode.BTC} />
+              Bitcoin
+            </Typography>
+            <Typography variant="h6">BTC</Typography>
+            <Chip label="#Rank 1" />
+          </Stack>
 
-        <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
-          <Typography variant="h4">$46,953.04</Typography>
-          <Typography variant="h6">
-            <ArrowDropUp sx={{ verticalAlign: "middle" }} />
-            2.51%
-          </Typography>
-          <Typography variant="h6">(24H)</Typography>
-        </Stack>
-        <Divider />
-        <Stack direction="row" spacing={2}>
-          <Typography variant="h5" sx={{ textTransform: "capitalize" }}>
-            Bitcoin Price Chart (USD)
-          </Typography>
-          <Chip label="1H" />
-          <Chip label="24H" />
-          <Chip label="7D" />
-          <Chip label="1M" />
-          <Chip label="3M" />
-          <Chip label="6M" />
-          <Chip label="1Y" />
-          <Chip label="ALL" />
-        </Stack>
-        <CryptoPriceLineChart />
-      </Paper>
+          <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
+            <Typography variant="h4">$46,953.04</Typography>
+            <Typography variant="h6">
+              <ArrowDropUp sx={{ verticalAlign: "middle" }} />
+              2.51%
+            </Typography>
+            <Typography variant="h6">(24H)</Typography>
+          </Stack>
+          <Divider />
+          <Stack direction="row" spacing={2}>
+            <Typography variant="h5" sx={{ textTransform: "capitalize" }}>
+              Bitcoin Price Chart (USD)
+            </Typography>
+            {chipLabels.map((labels, index) => (
+              <Chip label={labels} key={index} />
+            ))}
+          </Stack>
+          <CryptoPriceLineChart />
+        </CardContent>
+      </Card>
     </Container>
   );
 };
