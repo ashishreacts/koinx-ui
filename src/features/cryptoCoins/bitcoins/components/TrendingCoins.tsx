433f243a7a7db375c7fa1476297e8b53609f8eb1
AJ5154 <ashishjadhav597@gmail.com>
2024-03-15 23:07:28 +0530

    complete the BrandAdvertisement component

--- src/features/cryptoCoins/bitcoins/components/TrendingCoins.tsx
+++ src/features/cryptoCoins/bitcoins/components/TrendingCoins.tsx
@@ -9,28 +9,38 @@ import {
 } from "@mui/material";
 
 export const TrendingCoins = () => {
-  const trendingCoinsData = [
-    { currencyCode: CurrencyCode.BTC, name: "Bitcoin (BTC)", percentage: 5.26 },
-    { currencyCode: CurrencyCode.BTC, name: "Bitcoin (BTC)", percentage: 6.26 },
-    { currencyCode: CurrencyCode.BTC, name: "Bitcoin (BTC)", percentage: 7.26 },
-  ];
   return (
     <Card>
       <CardHeader
         title={<Typography variant="h4">Trending Coins (24h)</Typography>}
       />
       <CardContent>
-        {trendingCoinsData.map((data, index) => (
-          <Stack direction="row" spacing={2} key={index}>
-            <CryptoIcon currencyCode={data.currencyCode} />
-            <Typography>{data.name}</Typography>
-            <Typography>
-              <ArrowDropUp sx={{ verticalAlign: "middle" }} />
-              {data.percentage}
-            </Typography>
-          </Stack>
-        ))}
+        <Stack direction="column" spacing={2}>
+          <TrendingCoin />
+          <TrendingCoin />
+          <TrendingCoin />
+        </Stack>
       </CardContent>
     </Card>
   );
 };
+
+function TrendingCoin() {
+  return (
+    <Stack
+      direction="row"
+      spacing={1}
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Stack direction="row" spacing={1} alignItems="center">
+        <CryptoIcon currencyCode={CurrencyCode.BTC} />
+        <Typography>Bitcoin (BTC)</Typography>
+      </Stack>
+      <Typography>
+        <ArrowDropUp sx={{ verticalAlign: "middle" }} />
+        2.51%
+      </Typography>
+    </Stack>
+  );
+}
