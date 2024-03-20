6c912deff29fcdc0f62c64fcccc94a9d96747080
AJ5154 <ashishjadhav597@gmail.com>
2024-03-20 17:41:33 +0530

    remove cardcontent and padding to grid

--- src/features/cryptoCoins/bitcoins/components/AboutCoinCardData.tsx
+++ src/features/cryptoCoins/bitcoins/components/AboutCoinCardData.tsx
@@ -2,7 +2,6 @@ import { ArrowForward } from "@mui/icons-material";
 import {
   Button,
   Card,
-  CardContent,
   Grid,
   ImageListItem,
   Stack,
@@ -23,25 +22,23 @@ export const AboutCoinCardData = ({ data }: AboutCoinCardDataProps) => {
         background: (theme) => theme.palette.highlight.light,
       }}
     >
-      <CardContent>
-        <Grid container>
-          <Grid item xs={5}>
-            <ImageListItem>
-              <img src={data.src} />
-            </ImageListItem>
-          </Grid>
-          <Grid item xs={7}>
-            <Stack direction="column" margin={1} spacing={2}>
-              <Typography textAlign="center" variant="h6">
-                {data.label}
-              </Typography>
-              <Button variant="contained" endIcon={<ArrowForward />}>
-                Check Now
-              </Button>
-            </Stack>
-          </Grid>
+      <Grid container padding={2}>
+        <Grid item xs={5}>
+          <ImageListItem>
+            <img src={data.src} />
+          </ImageListItem>
         </Grid>
-      </CardContent>
+        <Grid item xs={7}>
+          <Stack direction="column" margin={1} spacing={2}>
+            <Typography textAlign="center" variant="h6">
+              {data.label}
+            </Typography>
+            <Button variant="contained" endIcon={<ArrowForward />}>
+              Check Now
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
     </Card>
   );
 };
